887b0933081704c1fd69afb1ddc08a04359aca7e
Hanendyo Wicaksono <kitonus@gmail.com>
2020-07-27 04:46:24 +0700

    Hello World!

--- test-springboot/src/main/java/com/jatis/training/springboot/testspringboot/TestSpringbootApplication.java
+++ test-springboot/src/main/java/com/jatis/training/springboot/testspringboot/TestSpringbootApplication.java
@@ -2,6 +2,7 @@ package com.jatis.training.springboot.testspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class TestSpringbootApplication {
@@ -9,5 +10,5 @@ public class TestSpringbootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestSpringbootApplication.class, args);
 	}
-
+	
 }
