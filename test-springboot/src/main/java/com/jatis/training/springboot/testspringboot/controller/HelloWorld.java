351eac082f7b30336c041568659949ca0f1cb323
Hanendyo Wicaksono <kitonus@gmail.com>
2020-07-27 05:32:03 +0700

    Init YML configurations

--- test-springboot/src/main/java/com/jatis/training/springboot/testspringboot/controller/HelloWorld.java
+++ test-springboot/src/main/java/com/jatis/training/springboot/testspringboot/controller/HelloWorld.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloWorld {
-
+	
 	@GetMapping
 	public String helloWorld() {
 		return "Hello World!";
